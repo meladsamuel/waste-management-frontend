4ba2b1ac342c3ceca4eb5b63d7c60e2b73419bc1
meladsamuel <meladsamuel2@gmail.com>
2021-05-05 00:19:44 +0200

    add some feature for drawer

--- src/container/layouts/main/ListCollapseMenu.jsx
+++ src/container/layouts/main/ListCollapseMenu.jsx
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 import { List } from '@material-ui/core';
 import CollapseMenu from './CollapseMenu';
 
-const NestedMenu = ({ menuItems }) => {
+const NestedMenu = ({ drawerOpen, menuItems }) => {
   return (
     <List>
       {menuItems &&
-        menuItems.map((items) => <CollapseMenu key={items.key} items={items} />)}
+        menuItems.map((items) => (
+          <CollapseMenu drawerOpen={drawerOpen} key={items.key} items={items} />
+        ))}
     </List>
   );
 };
 NestedMenu.propTypes = {
   menuItems: PropTypes.arrayOf(PropTypes.any).isRequired,
+  drawerOpen: PropTypes.bool.isRequired,
 };
 export default NestedMenu;
