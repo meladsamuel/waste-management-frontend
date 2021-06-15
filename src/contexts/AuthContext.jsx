82e1d7a353a9a77d6af87433d4d910100553bb82
meladsamuel <meladsamuel2@gmail.com>
2021-06-15 23:58:54 +0200

    add authentication feature for the app

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,47 +1,96 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+import jwt from 'jwt-decode';
+import { useHistory } from 'react-router-dom';
+import api, { useGet } from '../api';
 
 const AuthContext = createContext(undefined);
 
 export const useAuth = () => useContext(AuthContext);
 
-const AuthProvider = ({ children }) => {
-  const [error, onError] = useState(null);
-  const [currentUser, setCurrentUser] = useState(null);
-  async function signUp(formData) {
-    try {
-      const { data } = await axios.post('http://localhost:5000/api/users', formData,
-        {headers: 'Access-Control-Allow-Origin: *'}
-        );
-      onError(null);
-      setCurrentUser(data.user);
-      return data.user;
-    } catch (err) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-      return null;
-    }
+export function hasAccess(appPermissions, userPermissions) {
+  for (let index = 0; index < appPermissions.length; index += 1) {
+    if (userPermissions.includes(appPermissions[index])) return true;
   }
+  return false;
+}
+
+const getToken = () => {
+  const localToken = localStorage.getItem('access_token');
+  if (localToken) {
+    return jwt(localToken);
+  }
+  return null;
+};
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(getToken());
+  const [errors, setErrors] = useState(null);
+  const [pending, setPending] = useState(false);
+  const { payload: userPayload, getByPK: getUser } = useGet('/users');
+  const history = useHistory();
+
+  function storeToken(accessToken) {
+    localStorage.setItem('access_token', accessToken);
+    const encodedToken = jwt(accessToken);
+    setToken(encodedToken);
+  }
+
+  const permissions = React.useMemo(() => {
+    if (token?.permissions) {
+      return token.permissions;
+    }
+    return [];
+  }, [token]);
+
+  useEffect(() => getUser(token?.sub), [token]);
+
+  async function signUp(formData) {
+    const { data } = await api.post('/users', formData);
+    localStorage.setItem('access_token', data.access_token);
+    localStorage.setItem('refresh_token', data.refresh_token);
+    const accessToken = jwt(data.access_token);
+    setToken(accessToken);
+    history.replace('/');
+  }
+
+  function signIn(formData, from) {
+    setPending(true);
+    api
+      .post('/users/auth', formData)
+      .then(({ data }) => {
+        localStorage.setItem('access_token', data.access_token);
+        localStorage.setItem('refresh_token', data.refresh_token);
+        const accessToken = jwt(data.access_token);
+        setToken(accessToken);
+        setPending(false);
+        setErrors(null);
+        history.replace(from);
+      })
+      .catch(({ response }) => {
+        setErrors(response.data);
+        setPending(false);
+      });
+  }
+
+  function logOut() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    setToken(null);
+  }
+
   const value = {
-    currentUser,
-    error,
+    token,
+    permissions,
+    pending,
+    errors,
+    user: userPayload?.user,
     signUp,
+    signIn,
+    logOut,
+    storeToken,
   };
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
