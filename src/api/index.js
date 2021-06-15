17dab5a4ae648ee56c761e190949621fdab9fcf9
meladsamuel <meladsamuel2@gmail.com>
2021-06-16 00:07:47 +0200

    add interceptors to handle refresh token

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,73 @@ import { useState } from 'react';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URI,
-  headers: { 'content-type': 'application/json' },
+  headers: {
+    'content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+  },
   responseType: 'json',
 });
+
+function getNewToken() {
+  return new Promise((resolve, reject) => {
+    axios
+      .post('http://localhost:5000/api/users/auth/refresh', null, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('refresh_token')}`,
+        },
+      })
+      .then(({ data }) => {
+        localStorage.setItem('access_token', data.access_token);
+        resolve({ token: data.access_token });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status !== 401) {
+      return new Promise((resolve, reject) => reject(error));
+    }
+    // Logout user if token refresh didn't work or user is disabled
+    if (
+      error.config.url === '/api/users/refresh' ||
+      error.response.message === 'Account is disabled.' ||
+      error.response.data.message === 'username or password not valid'
+    ) {
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('refresh_token');
+      return new Promise((resolve, reject) => reject(error));
+    }
+    const responseData = getNewToken()
+      .then(({ token }) => {
+        // New request with new token
+        const { config } = error;
+        config.headers.Authorization = `Bearer ${token}`;
+        localStorage.setItem('access_token', token);
+
+        return new Promise((resolve, reject) => {
+          api
+            .request(config)
+            .then((response) => {
+              resolve(response);
+            })
+            .catch((dataError) => {
+              reject(dataError);
+            });
+        });
+      })
+      .catch((tokenError) => {
+        Promise.reject(tokenError);
+      });
+    return responseData;
+  }
+);
+
 export const useGet = (resources) => {
   const [payload, setPayload] = useState(null);
   const [error, onError] = useState(null);
@@ -39,34 +103,35 @@ export const useCreate = (resources, onCreated) => {
       .then(({ data }) => {
         setPayload(data);
         setIsPending(false);
-        onCreated();
+        onCreated(data);
       })
       .catch((err) => {
         onError(err);
         setIsPending(false);
       });
   };
-  return { isPending, error, payload, create, setPayload };
+  return { isPending, error, payload, create, setPayload, onError };
 };
 
-export const useUpdate = (resources) => {
+export const useUpdate = (resources, onSuccess) => {
   const [payload, setPayload] = useState(null);
   const [error, onError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const update = (formData, primaryKey = null) => {
     setIsPending(true);
     api
-      .update(primaryKey ? `${resources}/${primaryKey}` : resources, formData)
+      .patch(primaryKey ? `${resources}/${primaryKey}` : resources, formData)
       .then(({ data }) => {
         setPayload(data);
         setIsPending(false);
+        onSuccess(data);
       })
       .catch((err) => {
         onError(err);
         setIsPending(false);
       });
   };
-  return { isPending, error, payload, update };
+  return { isPending, error, payload, update, setPayload };
 };
 
 export const useDelete = (resources, onDeleted) => {
@@ -80,14 +145,30 @@ export const useDelete = (resources, onDeleted) => {
       .then(({ data }) => {
         setPayload(data);
         setIsPending(false);
-        onDeleted();
+        onDeleted(primaryKey);
       })
       .catch((err) => {
         onError(err);
         setIsPending(false);
       });
   };
-  return { isPending, error, payload, deleteItem };
+  const deleteItems = (formData, primaryKey) => {
+    setIsPending(true);
+    api
+      .delete(primaryKey ? `${resources}/${primaryKey}` : resources, {
+        data: formData,
+      })
+      .then(({ data }) => {
+        setPayload(data);
+        setIsPending(false);
+        onDeleted(data);
+      })
+      .catch((err) => {
+        onError(err);
+        setIsPending(false);
+      });
+  };
+  return { isPending, error, payload, deleteItem, deleteItems };
 };
 
 export const useSend = (resources) => {
