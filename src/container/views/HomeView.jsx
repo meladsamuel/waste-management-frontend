1200ae3bed200e89727f401f3ec9bd4cfd3dbac2
meladsamuel <meladsamuel2@gmail.com>
2021-06-16 00:06:11 +0200

    update home view

--- src/container/views/HomeView.jsx
+++ src/container/views/HomeView.jsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { Link } from '@material-ui/core';
-// import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core';
+import Map from '../../component/Map';
+import { useGet } from '../../api';
+import mapStyle from '../../mapStyle';
+
+const useStyles = makeStyles((theme) => ({
+  mapContainer: {
+    flex: 1,
+    position: 'relative',
+    '@media (min-width:0px) and (orientation: landscape)': {
+      height: `calc(100% - ${theme.mixins.toolbar['@media (min-width:0px) and (orientation: landscape)'].minHeight}px)`,
+    },
+    '@media (min-width:600px)': {
+      height: `calc(100% - ${theme.mixins.toolbar['@media (min-width:600px)'].minHeight}px)`,
+    },
+    height: `calc(100% - ${theme.mixins.toolbar.minHeight}px)`,
+  },
+  '@global': {
+    'html, body, #root': {
+      height: '100%',
+    },
+  },
+}));
 
 const Home = () => {
+  const classes = useStyles();
+
+  const { payload: baskets, getAll: getAllBasket } = useGet('/baskets');
+  const [position, setPosition] = React.useState(null);
+  const options = React.useMemo(() => {
+    return {
+      disableDefaultUI: true,
+      styles: mapStyle,
+      center: position,
+      zoom: 16,
+    };
+  }, [position]);
+  useEffect(getAllBasket, []);
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(({ coords }) => {
+        setPosition({ lat: coords.latitude, lng: coords.longitude });
+      });
+    }
+  }, []);
+
   return (
-    <>
-      <Link component={RouterLink} to="/dashboard">
-        dashboard
-      </Link>
-      <Link component={RouterLink} to="/register">
-        SignUp
-      </Link>
-    </>
+    <div className={classes.mapContainer}>
+      <Map data={baskets} options={options} />
+    </div>
   );
 };
 
