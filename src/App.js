9ae0b93e41e27977c0b158d5f1ec1280863b32f1
meladsamuel <meladsamuel2@gmail.com>
2021-06-16 00:00:59 +0200

    add authentication feature to the app

--- src/App.js
+++ src/App.js
@@ -6,30 +6,95 @@ import ClientLayout from './container/layouts/client';
 import HomeView from './container/views/HomeView';
 import MapView from './container/views/MapView';
 import NotFoundView from './container/views/NotFoundView';
+import AuthProvider from './contexts/AuthContext';
+import SignUp from './component/SignUp';
+import Login from './component/Login';
+import AuthRoute from './component/AuthRoute';
+import GuardRoute from './component/GuardRoute';
+import UserView from './container/views/UserView';
+import RoleView from './container/views/RoleView';
+import PermissionView from './container/views/PermissionView';
+import RoleDetailsView from './container/views/RoleDetailsView';
+import ConfirmationView from './container/views/ConfirmationView';
+import ProfileView from './container/views/ProfileView';
+import Activate from './component/Activate';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <ClientLayout>
-            <HomeView />
-          </ClientLayout>
-        </Route>
-        <Route path="/dashboard">
-          <MainLayout>
-            <Typography variant="h3">Dashborad Page</Typography>
-          </MainLayout>
-        </Route>
-        <Route path="/baskets/:action">
-          <MainLayout hasPadding={false}>
-            <MapView />
-          </MainLayout>
-        </Route>
-        <Route>
-          <NotFoundView />
-        </Route>
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <AuthRoute exact path="/">
+            <ClientLayout>
+              <HomeView />
+            </ClientLayout>
+          </AuthRoute>
+          <AuthRoute exact path="/profile">
+            <ClientLayout>
+              <ProfileView />
+            </ClientLayout>
+          </AuthRoute>
+          <AuthRoute path="/dashboard" appPermissions={['basket:read']}>
+            <MainLayout>
+              <Typography variant="h3">Dashborad Page</Typography>
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute
+            path="/baskets/:action"
+            appPermissions={[
+              'basket:read',
+              'basket:create',
+              'basket:update',
+              'basket:software-update',
+            ]}
+          >
+            <MainLayout hasPadding={false}>
+              <MapView />
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute path="/users/show" appPermissions={['users:read']}>
+            <MainLayout>
+              <UserView />
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute exact path="/roles" appPermissions={['roles:read']}>
+            <MainLayout>
+              <RoleView />
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute exact path="/roles/:role" appPermissions={['role:read']}>
+            <MainLayout>
+              <RoleDetailsView />
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute path="/permissions" appPermissions={['permissions:read']}>
+            <MainLayout>
+              <PermissionView />
+            </MainLayout>
+          </AuthRoute>
+          <AuthRoute path="/confirm" exclude>
+            <ClientLayout>
+              <ConfirmationView />
+            </ClientLayout>
+          </AuthRoute>
+          <Route path="/activate">
+            <Activate />
+          </Route>
+          <GuardRoute path="/register">
+            <ClientLayout>
+              <SignUp />
+            </ClientLayout>
+          </GuardRoute>
+          <GuardRoute path="/login">
+            <ClientLayout>
+              <Login />
+            </ClientLayout>
+          </GuardRoute>
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </AuthProvider>
     </Router>
   );
 }
