36381323de9d290db821a28d6fa920c67fe57fd9
meladsamuel <meladsamuel2@gmail.com>
2021-06-15 23:59:44 +0200

    add view of confirmation email

---
+++ src/container/views/ConfirmationView.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Typography, Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  actionSection: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+const ConfirmationView = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <img width="200" src="/verify_email.svg" alt="verify your email" />
+      <Typography variant="h2" align="center">
+        Please verify your email address
+      </Typography>
+      <Typography paragraph align="center">
+        Before using our waste management application, we need you to verify your
+        email address.
+      </Typography>
+      <Typography paragraph align="center">
+        An email containing verification instructions was sent to{' '}
+        <strong>meladsamuel@meladsamuel.com</strong>.
+      </Typography>
+      <div className={classes.actionSection}>
+        <Button variant="outlined" color="primary">
+          Resend verification email
+        </Button>
+        <Button variant="outlined" color="primary">
+          Change email
+        </Button>
+      </div>
+    </Container>
+  );
+};
+
+export default ConfirmationView;
